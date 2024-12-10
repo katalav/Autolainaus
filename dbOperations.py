a3c145ae3f99b73f02c9c23897f7957041091a2a
Mika Vainio <koti.mika.vainio@gmail.com>
2024-12-10 14:04:15 +0200

    tietokantamoduulin muutokset 10.12

--- dbOperations.py
+++ dbOperations.py
@@ -4,7 +4,15 @@
 # KIRJASTOT JA MODUULIT
 # ---------------------
 
+# Ladattavat kirjastot
 import psycopg2
+
+# Sisäiset kirjastot
+import json
+
+# Omat moduulit
+import cipher
+
 
 # LUOKAT
 # ------
@@ -17,8 +25,91 @@ class DbConnection():
         self.server = settings['server']
         self.port = settings['port']
         self.databaseName = settings['database']
-        self.userName = settings['username']
+        self.userName = settings['userName']
         self.password = settings['password']
+        self.connectionString = f'database={self.databaseName}, user={self.userName}, password={self.password}, server={self.server}, port={self.port}'
+
+
+    # Metodi tietojen lisäämiseen (INSERT)
+    def addToTable(self, table: str, data: dict) -> str:
+        """Inserts a record (row) to a table according to a dictionary
+        containing field names (columns) as keys and values
+
+        Args:
+            table (str): Name of the table
+            data (dict): Field names and values
+
+        Returns:
+            str: Information about successfull operation or an error message
+        """
+
+        # Muodostetaan lista sarakkeiden (kenttien) nimistä ja arvoista SQL laustetta varten
+        keys = data.keys() # Luetaan sanakirjan avaimet
+        columnList = [] # Alustetaan sarakelista tyhjäksi
+        valueList = [] # Alusetaan arvolista tyhjäksi
+
+        # Luetaan kaikki avaimet sekä arvot ja lisätään ne listoihin
+        for key in keys:
+            columnList.append(key)
+            valueList.append(data[key])
+
+        # Määritellään tilaviestiksi tyhjä merkkijono
+        message = ''
+
+        # Yritetään avata yhteys tietokantaan ja lisätä tietue
+        try:
+            # Luodaan yhteys tietokantaan
+            # currentConnection = psycopg2.connect(self.connectionString)
+
+            # Luodaan kursori suorittamaan tietokantoperaatiota
+            # cursor = currentConnection.cursor()
+
+            # Määritellään SQL-lause suoritettavaksi
+            columns = ''
+            values = ''
+            for column in columnList:
+                columns += column + ', '
+
+            # TODO: Tee tähän toiminto, joka siistii viimeisen ,:n ja välilyönnin pois merkkijonosta
+            sqlClause = f'INSERT INTO {table} ({columns}) VALUES ({valueList})'
+            print(sqlClause)
+            # Suoritetaan SQL-lause
+            #cursor.execute(sqlClause)
+
+            # Vahvistetaan tapahtuma (transaction)
+           # currentConnection.commit()
+
+        except (Exception, psycopg2.Error) as e:
+            message = 'Tietokantayhteyden muodostamisesa tapahtui virhe: ' + str(e)
+            
+        finally:
+            if message == '':
+                message = f'Tietue lisättiin tauluun {table}'
+
+            # Selvitetään muodostuiko yhteysolio
+            #if currentConnection:
+                #cursor.close() # Tuhotaan kursori
+                #currentConnection.close() # Tuhotaan yhteys
+
+            return message
+        
+if __name__ == "__main__":
+
+    testDictionary = {'server': '127.0.0.1',
+                      'port': '5432',
+                      'database': 'testi',
+                      'userName': 'user',
+                      'password': 'salasana'}    
+    
+    tableDictionary = {'Etunimi': 'Erkki',
+                       'Sukunimi': 'Esimerkki'}
+    
+    
+    dbConnection = DbConnection(testDictionary)
+
+    print('Yhteysmerkkijono on:', dbConnection.connectionString)
+
+    dbConnection.addToTable('testitaulu', tableDictionary)
     
 
     
