29fdca661fbc2246b564c20fd8f0eb4009d3d006
katalav <194138161+katalav@users.noreply.github.com>
2025-02-07 20:48:36 +0200

    Delete SQL/ryhma.sql

--- SQL/ryhma.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM public.ryhma
-ORDER BY ryhma ASC
-CREATE TABLE ryhma (
-                ryhma VARCHAR(50) NOT NULL,
-                vastuuhenkilo VARCHAR(50),
-                CONSTRAINT ryhma_pk PRIMARY KEY (ryhma)
-)
